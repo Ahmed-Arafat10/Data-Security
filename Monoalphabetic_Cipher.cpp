391ca8f09d7e9309cd994aa66a426dd431fb7d9d
AhmedArafat <63191956+Ahmed-Arafat10@users.noreply.github.com>
2021-12-04 23:48:01 +0200

    Update Monoalphabetic_Cipher.cpp

--- Monoalphabetic_Cipher.cpp
+++ Monoalphabetic_Cipher.cpp
@@ -1,10 +1,16 @@
 #include <bits/stdc++.h>
 typedef long long ll;
 using namespace std;
+string ToLowChar(string str, ll sz)
+{
+    for(ll i = 0; i<sz; i++) str[i] = tolower(str[i]);
+    return str;
+}
 int main()
 {
     string Plain;
     getline(cin,Plain);
+    Plain = ToLowChar(Plain,(ll)Plain.size());
     srand((unsigned) time(NULL));
     vector<char>v1;
     for(char c = 'a'; c<='z'; c++)v1.push_back(c);
@@ -16,7 +22,8 @@ int main()
         //cout<<Random<<" ";
         auto it = v1.begin()+Random;
         Cipher += v1[Random];
-        v1.erase(it);
+        swap(v1[v1.size()-1],v1[Random]);
+        v1.erase(v1.end()-1);
     }
     cout<<"Plain: "<<Plain<<endl;
     cout<<"Cipher: "<<Cipher<<endl;
@@ -31,4 +38,3 @@ int main()
     }
 
 }
-

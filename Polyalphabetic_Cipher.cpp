6d09473b224b177b9c00cf806b630e0e7fffa610
AhmedArafat <63191956+Ahmed-Arafat10@users.noreply.github.com>
2021-12-05 19:23:56 +0200

    Update Polyalphabetic_Cipher.cpp

--- Polyalphabetic_Cipher.cpp
+++ Polyalphabetic_Cipher.cpp
@@ -6,11 +6,29 @@ string ToLowChar(string str, ll sz)
     for(ll i = 0; i<sz; i++) str[i] = tolower(str[i]);
     return str;
 }
+void Encryption(ll __P,ll __C)
+{
+    ll __X = __P + __C;
+    char __R = 'a'+ (__X % 26);
+    cout<<__R;
+}
+string Decryption(ll __P,ll __C)
+{
+    ll __X = __P - __C;
+    if(__X < 0) __X += 26;
+    char __R = 'a'+ (__X % 26);
+    cout<<__R;
+}
 int main()
 {
+    char Type;
     string Plain,Key;
+    printf("Please Enter Plain/Chiper Text:\n");
     getline(cin,Plain);
+    printf("Please Enter Key:\n");
     cin>>Key;
+    printf("Please Enter (e) o Encrypt or (d) to Decrypt:\n");
+    cin>>Type;
     Plain = ToLowChar(Plain,(ll)Plain.size());
     Key = ToLowChar(Key,(ll)Key.size());
     string Temp = Key;
@@ -25,15 +43,21 @@ int main()
     {
         ll __P = Plain[i] - 'a';
         ll __C = Key[i] - 'a' ;
-        ll __X = __P + __C;
-        char __R = 'a'+ (__X % 26);
-        cout<<__R;
+        if(Type =='e') Encryption(__P,__C);
+        else Decryption(__P,__C);
     }
 }
 /*
 I/P:
 wearediscoveredsaveyourself
+deceptive
+e
 ----
 O/P:
-deceptive
+zicvtwqngrzgvtwavzhcqyglmgj
+
+
+TIHJRG
+ART
+d
 */
